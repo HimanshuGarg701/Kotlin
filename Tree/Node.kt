6243137e104db5ff6ba6b3af6485b441fe2146ee
Himanshu Garg <hgarg701@gmail.com>
2020-08-01 01:26:42 -0700

    Adding elements to tree

---
+++ Tree/Node.kt
@@ -0,0 +1,4 @@
+package Tree
+
+class Node(var data: Int, var left : Node?=null, var right: Node?=null) {
+}
---
+++ Tree/Tree.kt
@@ -0,0 +1,34 @@
+package Tree
+
+class Tree {
+
+    var root : Node? = null;
+
+    fun addNode(data : Int){
+        if(root==null){
+            root = Node(data);
+        }else{
+            addHelper(root!!, data);
+        }
+    }
+
+    private fun addHelper(node : Node?, data: Int) : Node{
+        if(node==null){
+            return Node(data);
+        }else if(data < node.data){
+            node.left = addHelper(node.left, data);
+        }
+        else{
+            node.right = addHelper(node.right, data);
+        }
+        return node;
+    }
+
+    fun print(){
+        if(root!=null){
+            printHelper(root!!);
+        }
+    }
+
+    private fun printHelper()
+}
