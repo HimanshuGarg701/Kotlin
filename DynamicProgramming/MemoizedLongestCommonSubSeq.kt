ba31bd183ba2a62a140c1cd11d1e2a9ec5809083
Himanshu Garg <hgarg701@gmail.com>
2020-08-10 23:19:16 -0700

    Memoized Longest common subsequence recursive

---
+++ DynamicProgramming/MemoizedLongestCommonSubSeq.kt
@@ -0,0 +1,26 @@
+package DynamicProgramming
+
+fun main(args:Array<String>){
+    val x = "abcdefgh"
+    val y = "acdbcdeh"
+    val matrix = Array(x.length+1){Array(y.length+1){-1} }
+    println("Longest subseq : ${longestSubSequence(x, y, x.length, y.length, matrix)}")
+}
+
+private fun longestSubSequence(x:String, y:String, xLen:Int, yLen:Int, matrix:Array<Array<Int>>):Int{
+    if(xLen==0 || yLen==0){
+        return 0
+    }
+
+    if(matrix[xLen][yLen]!=-1){
+        return matrix[xLen][yLen]
+    }
+
+    if(x[xLen-1] == y[yLen-1]){
+        return 1+ longestSubSequence(x,y, xLen-1, yLen-1, matrix)
+    }else{
+        matrix[xLen][yLen] = Math.max(longestSubSequence(x,y,xLen-1, yLen, matrix),
+                                        longestSubSequence(x,y,xLen,yLen-1, matrix))
+        return matrix[xLen][yLen]
+    }
+}
